78d0eac497254a552535b6b0bf67e9422171e9d9
dingzhenhua <zhenhua.ding@ly.com>
2019-05-08 15:01:58 +0800

    添加生成随机字串的帮助类

---
+++ StarrySky.DZH.Util/Common/RandomStringHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarrySky.DZH.Util.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class RandomStringHelper
+    {
+        ///<summary>
+        ///生成随机字符串 
+        ///</summary>
+        ///<param name="length">目标字符串的长度</param>
+        ///<param name="useNum">是否包含数字，</param>
+        ///<param name="useLow">是否包含小写字母，</param>
+        ///<param name="useUpp">是否包含大写字母，</param>
+        ///<param name="useSpe">是否包含特殊字符，默认为不包含</param>
+        ///<returns>指定长度的随机字符串</returns>
+        public static string GetRandomString(int length, bool useNum = true, bool useLow = true, bool useUpp = true, bool useSpe = false)
+        {
+            byte[] b = new byte[4];
+            new System.Security.Cryptography.RNGCryptoServiceProvider().GetBytes(b);
+            Random r = new Random(BitConverter.ToInt32(b, 0));
+            string s = null, str = "";
+            if (useNum == true) { str += "0123456789"; }
+            if (useLow == true) { str += "abcdefghijklmnopqrstuvwxyz"; }
+            if (useUpp == true) { str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; }
+            if (useSpe == true) { str += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"; }
+            for (int i = 0; i < length; i++)
+            {
+                s += str.Substring(r.Next(0, str.Length - 1), 1);
+            }
+            return s;
+        }
+    }
+}
