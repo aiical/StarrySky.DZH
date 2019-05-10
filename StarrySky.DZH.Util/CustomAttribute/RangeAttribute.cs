667bfaea8e247d0f905289a38603af87e78047d0
dingzhenhua <zhenhua.ding@ly.com>
2019-05-10 15:02:13 +0800

    特性类通用验证组件开发

---
+++ StarrySky.DZH.Util/CustomAttribute/RangeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarrySky.DZH.Util.CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class RangeAttribute : Attribute
+    {
+        private int _minLength;
+
+        private int _maxLength;
+
+        public RangeAttribute(int minLength, int maxLength)
+        {
+            _minLength = minLength;
+            _maxLength = maxLength;
+        }
+    }
+
+}
---
+++ StarrySky.DZH.WebDemo/CustomAttribute/MyActionFilterAttribute.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace StarrySky.DZH.WebDemo.CustomAttribute
+{
+    /// <summary>
+    /// Action执行会触发（绑定了这个）
+    /// </summary>
+    public class MyActionFilterAttribute : ActionFilterAttribute
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="actionExecutedContext"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            string memberId = "";
+            string jsonString = "";
+
+            try
+            {
+                //执行签到
+                // var loginRequest = actionExecutedContext.ActionContext.ActionArguments.Values.FirstOrDefault() as LoginRequest;
+                jsonString = actionExecutedContext.Response.Content.ReadAsStringAsync().Result;
+                //ResultMessage<WeiXinMemberInfoResponse> response = JsonConvert.DeserializeObject<ResultMessage<WeiXinMemberInfoResponse>>(jsonString);
+
+                return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+        }
+
+
+        /// <summary>
+        /// 读取action返回的result
+        /// </summary>
+        /// <param name="actionExecutedContext"></param>
+        /// <returns></returns>
+        public string GetResponseValues(HttpActionExecutedContext actionExecutedContext)
+        {
+            return actionExecutedContext.Response.Content.ReadAsStringAsync().Result;
+            //
+            /*
+            这个StreamReader不能关闭，也不能dispose， 关了就傻逼了
+            因为你关掉后，后面的管道  或拦截器就没办法读取了
+            */
+            //var reader = new StreamReader(stream, encoding);
+            //string result = reader.ReadToEnd();
+            /*
+            这里也要注意：   stream.Position = 0; 
+            当你读取完之后必须把stream的位置设为开始
+            因为request和response读取完以后Position到最后一个位置，交给下一个方法处理的时候就会读不到内容了。
+            */
+            //stream.Position = 0;
+            //return result;
+        }
+
+    }
+}
