b6ad30fcb8193cba96780202a3bdf2ed5097d93f
dingzhenhua <zhenhua.ding@ly.com>
2019-10-08 15:40:25 +0800

    小维护

---
+++ StarrySky.DZH.Util/Common/DateTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarrySky.DZH.Util.Common
+{
+    public class DateTimeHelper
+    {
+        
+        /// <summary>
+        /// 获取当天晚的秒数
+        /// </summary>
+        /// <returns></returns>
+        public static double GetTodayExpireSeconds()
+        {
+            return (DateTime.Today.AddDays(1) - DateTime.Now).TotalSeconds;
+        }
+    }
+}
--- StarrySky.DZH.Util/Extensions/DateTimeExtend.cs
+++ StarrySky.DZH.Util/Extensions/DateTimeExtend.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,5 +34,44 @@ namespace StarrySky.DZH.Util.Extensions
             TimeSpan ts = datetime - new DateTime(1970, 1, 1, 0, 0, 0, 0);
             return Convert.ToInt64(ts.TotalSeconds).ToString();
         }
+
+        /// <summary>
+        /// 转到固定格式 :dateFormat 0 返回 "yyyy/MM/dd HH:mm:ss", 1 返回"yyyy-MM-dd HH:mm:ss"
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="dateFormat">0 返回 "yyyy/MM/dd HH:mm:ss" ,1 返回"yyyy-MM-dd HH:mm:ss"</param>
+        /// <returns></returns>
+        public static string ToFixFormat(this DateTime time, int dateFormat = 0)
+        {
+            if (dateFormat == 0)
+            {
+                return time.ToString("yyyy/MM/dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            }
+            else if (dateFormat == 1)
+            {
+                return time.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            }
+            return time.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+        }
+
+
+        /// <summary>
+        /// 本周第一天
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static DateTime FirstDayOfWeek(this DateTime time)
+        {
+            int dayCount = (int)time.DayOfWeek - 1;
+            if (dayCount >= 0)
+            {
+                return time.AddDays(-1 * dayCount);
+            }
+            else
+            {
+                return time.AddDays(-6);
+            }
+        }
+
     }
 }
