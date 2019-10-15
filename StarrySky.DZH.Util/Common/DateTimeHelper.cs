80123ab9c5e22182ec67c996c3aecd58c992df52
dingzhenhua <zhenhua.ding@ly.com>
2019-10-15 17:11:28 +0800

    小维护

--- StarrySky.DZH.Util/Common/DateTimeHelper.cs
+++ StarrySky.DZH.Util/Common/DateTimeHelper.cs
@@ -10,12 +10,25 @@ namespace StarrySky.DZH.Util.Common
     {
         
         /// <summary>
-        /// 获取当天晚的秒数
+        /// 获取截止到当天24点的总秒数
         /// </summary>
         /// <returns></returns>
-        public static double GetTodayExpireSeconds()
+        public static double GetTodayRestSeconds()
         {
             return (DateTime.Today.AddDays(1) - DateTime.Now).TotalSeconds;
+        }
+
+
+        /// <summary>
+        /// 将Unix时间戳转成DateTime
+        /// </summary>
+        /// <param name="timestamp">13位或10位时间戳</param>
+        /// <returns>DateTime</returns>
+        public static DateTime ToDateTime(long timestamp)
+        {
+            var ticks = DateTime.Now.ToUniversalTime().Ticks - (timestamp * (timestamp.ToString().Length == 13 ? 10000 : 10000000) + 621355968000000000);
+            TimeSpan ts = new TimeSpan(ticks);
+            return DateTime.Now.Add(-ts);
         }
     }
 }
